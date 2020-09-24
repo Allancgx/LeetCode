1d8ca5e161ca2df207d701e7da5908a58182f137
Allan <allancgx@gmail.com>
2020-09-23 21:13:54 -0400

    Added a new java class: findMaxConsecutiveOnes and emptied Main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,4 @@
 package com.company;
-//https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3238/
-//Max Consecutive Ones
+
 public class Main {
-
-    public static int findMaxConsecutiveOnes(int[] nums) {
-        int longest = 0;
-        int current = 0;
-        for(int each: nums){
-            if (each == 1){
-                current++;
-            }else{
-                current = 0;
-            }
-            if (current > longest){
-                longest = current;
-            }
-        }
-        return longest;
-    }
-
-    public static void main(String[] args) {
-        int[] input = {1,1,0,1,1,1};
-	System.out.println(findMaxConsecutiveOnes(input));
-    }
 }
